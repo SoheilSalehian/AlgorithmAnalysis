6c2fde09f73e91a6774e2d37b4e7c6e2c0fc2191
soheil <soheil@ubuntu.(none)>
2013-03-07 14:29:33 -0800

    Fixed data structure from two lists to OrderedDictionary

--- src/DijkstraShortPath/dijkistra.py
+++ src/DijkstraShortPath/dijkistra.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python
 
+from collections import OrderedDict
+
+    
 
 
 
+#Store items in the order the keys were last added
+class LastUpdatedOrderedDict(OrderedDict):
+    def __setitem__(self, key, value):
+        if key in self:
+            del self[key]
+        OrderedDict.__setitem__(self, key, value)
 
-#list of processed vertices so far
-verticesProcessed = [1]
+#initialize the dictionary to have source vertex 1 with distance 0       
+exploredPathDistanceDic = {1:0}        
+#ordered dictionary by vertices (key)
+exploredPathDistanceDic = LastUpdatedOrderedDict(OrderedDict(exploredPathDistanceDic.items()))
 
-#computed shortest path distance list with index of the processed vertices
-shortestPathDistance = [0]
-
+#book keeping parameters
 numberOfNodes = 0
+finalNode = 0
 
 #while verticesProcessed != not yet explored vertices
 #    among all edges that have their tail in verticesProcessed and head not in there:
@@ -20,18 +30,19 @@ numberOfNodes = 0
 #set shortestPathDistance[new chosen]= shortestPathDistance[everything previously in verticesProcessed] + distance from the current vertex to the newly chosen vertex  
 
 
-def dijkstraLoop(edgesDic):
-    
-    while(len(verticesProcessed) < numberOfNodes):
+def dijkstraLoop(edgesDic, finalNode):
+    print "final node", finalNode
+
+    while(next(reversed(exploredPathDistanceDic)) != finalNode):
         length = 0
         dGreedyCriteriaDic = {}
-        #our current node is the last element from the verticesProcessed list
-        currentNode = verticesProcessed[-1]
+        #our current node is the last element from the ordered exploredDistanceDic
+        currentNode = next(reversed(exploredPathDistanceDic))
         print "currentNode", currentNode
         #read the source nodes from edgesDic
         for node in edgesDic.keys():
             #find among the dictionary keys, the current node and the appropriate adjacents such that they are not processed yet
-            if (node[0] == currentNode and not node[1] in verticesProcessed):
+            if (node[0] == currentNode):
                 #store the possible outgoing edge lengths
                 length= edgesDic[node]
                 #sort the lengths to find the minimum
@@ -39,22 +50,34 @@ def dijkstraLoop(edgesDic):
             else:
                 #else skip and iterate over the next adjacent
                 continue
-            #Dijkstra's greedy criteria calculated as the sum of current shortest path and minimum possible outgoing edge
-            dGreedyCriteriaDic[node[1]] = shortestPathDistance[currentNode-1] + length
-#                print dGreedyCriteriaDic
-            #extract the key with the minimum Dijkstra's greedy criteria value to be the chosen one!!
-            chosenNode = min(dGreedyCriteriaDic, key=dGreedyCriteriaDic.get)
-#                print "greedy",chosenNode
-        verticesProcessed.append(chosenNode)
-        shortestPathDistance.append(dGreedyCriteriaDic[chosenNode])
-        print shortestPathDistance
+            #Dijkstra's greedy criteria calculated as the sum of current shortest path (value of the latest key in exploredPathDistanceDic) and minimum possible length of outgoing edge
+            dGreedyCriteriaDic[node[1]] = next(reversed(exploredPathDistanceDic.values())) + length
+            print "greedy",dGreedyCriteriaDic
+            
+        #if we there is no way from the currentNode to go we are have scanned shortpaths as much as we could, now need to start again
+        
+        #extract the key with the minimum Dijkstra's greedy criteria value to be the chosen one!!
+        chosenNode = min(dGreedyCriteriaDic, key=dGreedyCriteriaDic.get)
+        
+        #check to see if we have traversed any edges at all
+        if(currentNode != chosenNode):
+            #if the chosenNode already exists and its current value is larger than what the criteria have found, take the newly found value
+            if (not chosenNode in exploredPathDistanceDic.keys() or exploredPathDistanceDic[chosenNode] > dGreedyCriteriaDic[chosenNode]):    
+                exploredPathDistanceDic[chosenNode] = dGreedyCriteriaDic[chosenNode]
+            #otherwise no need to update the dictionary for this node, return
+            else:
+                return
+        #if we haven't then this is as far as we could go for this node    
+        else:
+            exploredPathDistanceDic[chosenNode] = 'inf'
+            return 
         
             
 
 # this function reads the data of a adjancey matrix text format and converts it into a hash with a tuple key of the two nodes 
 # and their corresponding weight as key
 def readData():
-    global numberOfNodes
+    global numberOfNodes, nodesLeft
     #keep the edge information in the hash edges 
     edgesDic = {}
     with open('/home/soheil/workspace/AlgorithmAnalysis/src/DijkstraShortPath/exampleInput.txt',"r") as fp:
@@ -68,15 +91,24 @@ def readData():
                 key = (int(nodes[0]),int(nodes[j].split(",")[0]))
                 #extract the weight information as value of the hash 
                 edgesDic[key] = int(nodes[j].split(",")[1])
-        numberOfNodes = i[0]
+    numberOfNodes = int(i[0])
     print numberOfNodes
     print edgesDic
     return edgesDic    
 
 
 
+
+
 if __name__ == '__main__':
+    print exploredPathDistanceDic
     edges = readData()
-    dijkstraLoop(edges)
+    
+    for i in xrange(numberOfNodes):
+        dijkstraLoop(edges,i+1)   
+        print sorted(exploredPathDistanceDic.items(), key=lambda x: x[0])
+    
+        
+
 
 
